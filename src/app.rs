424f03d644104a021efcdd12fc542161da6b36f9
n-k <nipunkumar@outlook.com>
2024-03-25 23:05:34 +0530

    WIP: ble

---
+++ src/app.rs
@@ -0,0 +1,43 @@
+use gattrs::{gatt::CharacteristicRegister, gatt_characteristic, gatt_service};
+
+#[gatt_service(uuid = "42000000-0000-0000-0000-000000000000")]
+pub struct CounterService {}
+
+impl CounterService {
+    fn get_characteristics(&self) -> Vec<impl CharacteristicRegister> {
+        vec![CountCharacteristic::default()]
+    }
+}
+
+#[gatt_characteristic(
+    uuid = "42000000-0000-0000-0000-000000000001",
+    flags = ["read", "write", "notify"]
+)]
+struct CountCharacteristic {
+    count: u8,
+}
+
+impl CountCharacteristic {
+    async fn read(&self) -> gattrs::GattReadResult {
+        Ok(vec![self.count])
+    }
+
+    async fn write(&mut self, val: &[u8]) -> gattrs::GattWriteResult {
+        let count = val.first().unwrap_or(&self.count).to_owned();
+        self.count = count;
+        Ok(())
+    }
+}
+
+// #[tokio::main]
+// async fn main() -> gattrs::zbus::Result<()> {
+//     GattApplication::new()
+//         .await
+//         .service(CounterService::default())
+//         .await
+//         .advertise()
+//         .serve()
+//         .await?;
+
+//     Ok(())
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
-use alsa::{pcm::*, Direction, Error, ValueOr, PCM};
+use alsa::{pcm::*, Direction, ValueOr, PCM};
 use ringbuf::HeapRb;
-use std::{thread, time::Duration};
+use std::thread;
 
-fn main() -> ! {
-    let rb = HeapRb::<i16>::new(8192 * 10);
+fn start_pcm_stream(device: &str, dir: Direction) -> anyhow::Result<PCM> {
+    let pcm = PCM::new(device, dir, false)?;
+    {
+        // For this example, we assume 44100Hz, one channel, 16 bit audio.
+        let hwp = HwParams::any(&pcm)?;
+        hwp.set_channels(1)?;
+        hwp.set_rate(44100, ValueOr::Nearest)?;
+        hwp.set_format(Format::u32())?;
+        hwp.set_access(Access::RWInterleaved)?;
+        pcm.hw_params(&hwp)?;
+    }
+    pcm.start()?;
+    Ok(pcm)
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let rb = HeapRb::<u32>::new(8192 * 10);
     let (mut prod, mut cons) = rb.split();
 
     thread::spawn(move || {
         let pcm_in = start_pcm_stream("default:1", Direction::Capture).unwrap();
-        let io_in = pcm_in.io_i16().unwrap();
-        let mut buf = [0i16; 1024];
+        let io_in = pcm_in.io_u32().unwrap();
+        let mut buf = [0u32; 1024];
         loop {
             io_in.readi(&mut buf).unwrap();
             prod.push_slice(&buf);
@@ -17,8 +33,8 @@ fn main() -> ! {
     });
     thread::spawn(move || {
         let pcm_out = start_pcm_stream("default:1", Direction::Playback).unwrap();
-        let io_out = pcm_out.io_i16().unwrap();
-        let mut buf = [0i16; 1024];
+        let io_out = pcm_out.io_u32().unwrap();
+        let mut buf = [0u32; 1024];
         loop {
             cons.pop_slice(&mut buf);
             io_out.writei(&buf).unwrap();
@@ -26,21 +42,17 @@ fn main() -> ! {
     });
 
     loop {
-        thread::sleep(Duration::from_millis(500));
+        thread::sleep(std::time::Duration::from_millis(500));
     }
-}
 
-fn start_pcm_stream(device: &str, dir: Direction) -> Result<PCM, Error> {
-    let pcm = PCM::new(device, dir, false)?;
-    {
-        // For this example, we assume 44100Hz, one channel, 16 bit audio.
-        let hwp = HwParams::any(&pcm)?;
-        hwp.set_channels(1)?;
-        hwp.set_rate(44100, ValueOr::Nearest)?;
-        hwp.set_format(Format::s16())?;
-        hwp.set_access(Access::RWInterleaved)?;
-        pcm.hw_params(&hwp)?;
-    }
-    pcm.start()?;
-    Ok(pcm)
+    // build and server GATT application
+    // GattApplication::new()
+    //     .await
+    //     .service(CounterService::default())
+    //     .await
+    //     .advertise()
+    //     .serve()
+    //     .await?;
+
+    Ok(())
 }
